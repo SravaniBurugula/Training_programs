5a013fb0cf151757c46d93a86535077b514264d8
Sravani Burugula <Sravani.Burugula1@ibm.com>
2025-05-13 17:03:08 +0530

    Initial Commit

--- eclipse_workspace/spring/src/main/java/com/ibm/spring/TextEditor.java
+++ eclipse_workspace/spring/src/main/java/com/ibm/spring/TextEditor.java
@@ -2,6 +2,9 @@ package com.ibm.spring;
 
 public class TextEditor {
 	private SpellChecker spellChecker;
+	public TextEditor() {
+		
+	}
 	public TextEditor(SpellChecker spellChecker) {
 	System.out.println("Inside TextEditor");
 	this.spellChecker=spellChecker;
@@ -9,4 +12,8 @@ public class TextEditor {
 	public void spellCheck() {
 		spellChecker.checkSpelling();
 	}
+	public void setSpellChecker(SpellChecker spellChecker) {
+		this.spellChecker = spellChecker;
+	}
+	
 }
